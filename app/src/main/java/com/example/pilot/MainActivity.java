196fd0aeef2c9a56e23115ba28ece00333a8995b
Cyrill <cyrillkuettel@gmail.com>
2021-10-15 11:33:27 +0200

    added non-root bluetooth permissions

--- app/src/main/java/com/example/pilot/MainActivity.java
+++ app/src/main/java/com/example/pilot/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.pilot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            System.out.println("does not");
+        } else { System.out.println("does not"); }
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+
+
     }
 }
