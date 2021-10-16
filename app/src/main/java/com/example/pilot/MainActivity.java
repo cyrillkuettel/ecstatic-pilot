eb35de11c37de175bcee21c232cf2e8fb3c5c749
Cyrill <cyrillkuettel@gmail.com>
2021-10-16 16:32:45 +0200

    button for start event created with handler

--- app/src/main/java/com/example/pilot/MainActivity.java
+++ app/src/main/java/com/example/pilot/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.pilot;
-
+import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_ENABLE_BT = 1;
+    /*
+    The REQUEST_ENABLE_BT constant passed to startActivityForResult()
+     is a locally-defined integer
+     that must be greater than or equal to 0.
+     The system passes this constant back to you in your onActivityResult() implementation
+      as the requestCode parameter.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,15 +26,19 @@ public class MainActivity extends AppCompatActivity {
 
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-            System.out.println("does not");
-        } else { System.out.println("does not"); }
+           Log.v(TAG, "Device does not support bluetooth");
+        } else {
+            Log.v(TAG, "Device supports bluetooth");
+        }
+
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
             startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
         }
+    }
 
-
-
+    public void startClickHandler(View target) {
+        // Do stuff
+        Log.v(TAG, "clicked on button");
     }
 }
