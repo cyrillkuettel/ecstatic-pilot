32e86dc2be6da7463c800e19701d75ed6e159866
Cyrill <cyrillkuettel@gmail.com>
2021-11-11 21:29:17 +0100

    der Feinschliff für Bluetooth

---
+++ app/src/main/java/simple/bluetooth/terminal/BuildConfig.java
@@ -0,0 +1,9 @@
+package simple_bluetooth_terminal;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+    public static final String APPLICATION_ID = "li.garteroboter.pren";
+    public static final String BUILD_TYPE = "debug";
+    public static final int VERSION_CODE = 1;
+    public static final String VERSION_NAME = "1.0";
+}
