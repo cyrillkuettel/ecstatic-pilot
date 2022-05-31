dff4915c0467f584e52ff487eba271386be3eca5
Cyrill <cyrillkuettel@gmail.com>
2022-05-31 22:49:13 +0200

    3rd connection option: Command

--- app/src/main/java/li/garteroboter/pren/socket/SocketType.java
+++ app/src/main/java/li/garteroboter/pren/socket/SocketType.java
@@ -1,12 +1,12 @@
 package li.garteroboter.pren.socket;
 
 public enum SocketType {
-    /* There are two different use cases for Websocket */
-    Text("999"), Binary("888");
+    /* different use cases for Websocket */
+    Text("999"), Binary("888"), Command("777");
 
-    public final String id;
+    public final String type;
 
-     SocketType(String id) {
-        this.id = id;
+    SocketType(String type) {
+        this.type = type;
     }
 }
